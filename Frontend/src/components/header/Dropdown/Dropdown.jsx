b5f65f9bfb5ee5155821cb4d2bec148aad025707
Echedey Henríquez Hernández <echedeyhh1989@gmail.com>
2024-11-16 14:09:30 +0000

    Modified the dropdown to change the name of the logged-in user and to close the dropdown when clicking outside.

--- Frontend/src/components/header/Dropdown/Dropdown.jsx
+++ Frontend/src/components/header/Dropdown/Dropdown.jsx
@@ -1,31 +1,41 @@
 import DropdownContent from "./DropdownContent";
+import { useState, useRef } from "react";
+import { useAuth } from "../../../hooks/AuthProvider";
+import useOnClickOutside from "../../../hooks/UseOnClickOutside";
 import './Dropdown.css';
-import { useState } from "react";
 
 const Dropdown = () => {
 
-    const [name, setName] = useState("Pedro Picapiedra")
+    /* const [name, setName] = useState("Pedro Picapiedra") */
     const [rol, setRol] = useState("Estudiante")
 
+    const { user } = useAuth();
     const [menuState, setMenuState] = useState(false);
+    const dropDownRef = useRef();
 
-    const changeMenu= () =>{
-        setMenuState (!menuState);
+    useOnClickOutside(dropDownRef, () => setMenuState(false));
+
+    const changeMenu = () => {
+        setMenuState(!menuState);
     }
 
-    return( 
+    const userName = user ? user.email.split('@')[0] : "Pedro Picapiedra";
+
+    return (
         <>
-            <a className="userZone" tabIndex={0} onClick={changeMenu} onKeyDown={(e) => {if(e.key === 'Enter') {changeMenu();}}}>
-                <div className="userZone_menu">
-                    <div className='userZone_userFoto' ></div>
-                    <div className='menuInfo'>
-                        <p className='menuInfo_name'>{name}</p>
-                        <p className='menuInfo_rol'>{rol}</p>
+            <div ref={dropDownRef}>
+                <a className="userZone" tabIndex={0} onClick={changeMenu} onKeyDown={(e) => { if (e.key === 'Enter') { changeMenu(); } }}>
+                    <div className="userZone_menu">
+                        <div className='userZone_userFoto' ></div>
+                        <div className='menuInfo'>
+                            <p className='menuInfo_name'>{userName}</p>
+                            <p className='menuInfo_rol'>{rol}</p>
+                        </div>
                     </div>
-                </div>
-                <i className='fi fi-rr-angle-small-down'></i>
-            </a>
-            {menuState && <DropdownContent />}
+                    <i className='fi fi-rr-angle-small-down'></i>
+                </a>
+                {menuState && <DropdownContent />}
+            </div>
         </>
     )
 }

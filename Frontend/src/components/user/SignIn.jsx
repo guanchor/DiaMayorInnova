df19ba0d652cbdf5e5c336031b47adddee609959
AlbertoGG20 <albertoguerrerogutierrez@alumno.ieselrincon.es>
2024-11-21 16:30:59 +0000

    log component modified and added css

--- Frontend/src/components/user/SignIn.jsx
+++ Frontend/src/components/user/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../../hooks/AuthProvider";
 import { useNavigate } from "react-router-dom";
+import "./Sign.css";
 
 const SignIn = () => {
   const [input, setInput] = useState({
@@ -10,34 +11,24 @@ const SignIn = () => {
   const navigate = useNavigate();
   const auth = useAuth();
 
-  /*   const handleSubmitEvent = (e) => {
-      e.preventDefault();
-      if (input.email !== "" && input.password !== "") {
-        auth.signInAction(input);
-        return;
-      }
-      console.log(input.email + " y " + input.password);
-      alert("Please provide a valid input");
-    }; */
-
   const handleSubmitEvent = (e) => {
     e.preventDefault();
     if (input.email !== "" && input.password !== "") {
       auth.signInAction(input)
-      .then((response) => {
-        console.log("Server response in SignIn.jsx:", response);
-        const user = response?.data?.data?.user;
-        console.log("User from response:", user);
-        if (user && user.featured_image) {
-          const avatarUrl = `http://localhost:3000${user.featured_image}`;
-          console.log("Avatar URL:", avatarUrl);
-          auth.setUserAvatarUrl(avatarUrl);
-        } else {
-          console.error("Response is missing user or featured_image");
-        }
-      }).catch((error) => {
-        console.error("Error during sign-in:", error);
-      });
+        .then((response) => {
+          console.log("Server response in SignIn.jsx:", response);
+          const user = response?.data?.data?.user;
+          console.log("User from response:", user);
+          if (user && user.featured_image) {
+            const avatarUrl = `http://localhost:3000${user.featured_image}`;
+            console.log("Avatar URL:", avatarUrl);
+            auth.setUserAvatarUrl(avatarUrl);
+          } else {
+            console.error("Response is missing user or featured_image");
+          }
+        }).catch((error) => {
+          console.error("Error during sign-in:", error);
+        });
       return;
     }
     console.log(input.email + " y " + input.password);
@@ -58,34 +49,43 @@ const SignIn = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmitEvent}>
-        <div className="form_control">
-          <label htmlFor="user-email">Email:</label>
-          <input
-            type="email"
-            id="user-email"
-            name="email"
-            placeholder="example@yahoo.com"
-            aria-describedby="user-email"
-            aria-invalid="false"
-            onChange={handleInput}
-          />
+      <div className="log__section">
+        <img className="log__img" src="/images/log.jpg" alt="users imagen" />
+        <div className="log__container">
+          <h1 className="log__tittle">Iniciar Sesión</h1>
+          <form className="log__form" onSubmit={handleSubmitEvent}>
+            <div className="inputs__wrapper">
+              <div className="form_control">
+                <label className="input__wrapper" htmlFor="user-email">Email:
+                  <input
+                    type="email"
+                    id="user-email"
+                    name="email"
+                    placeholder="example@yahoo.com"
+                    aria-describedby="user-email"
+                    aria-invalid="false"
+                    onChange={handleInput}
+                  />
+                </label>
+              </div>
+              <div className="form_control">
+                <label className="input__wrapper" htmlFor="password">Password:
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    aria-describedby="user-password"
+                    aria-invalid="false"
+                    onChange={handleInput}
+                  />
+                </label>
+              </div>
+            </div>
+            {auth.error && <div style={{ color: "red" }}>{auth.error}</div>}
+            <button className="btn light">Iniciar Sesión</button>
+          </form>
         </div>
-        <div className="form_control">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            aria-describedby="user-password"
-            aria-invalid="false"
-            onChange={handleInput}
-          />
-        </div>
-        {auth.error && <div style={{ color: "red" }}>{auth.error}</div>}
-        <button className="btn-submit">Iniciar Sesión</button>
-      </form>
-      <button onClick={handleClick}>Ir a Inicio</button>
+      </div>
     </>
   );
 };

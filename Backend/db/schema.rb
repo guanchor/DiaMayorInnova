3a43a6719ffc2749650742aeb99dc4cceabf2ba1
AlbertoGG20 <albertoguerrerogutierrez@alumno.ieselrincon.es>
2024-12-04 15:19:13 +0000

    changes schema

--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_02_193006) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_03_194234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -137,6 +137,20 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_02_193006) do
     t.index ["user_id"], name: "index_statements_on_user_id"
   end
 
+  create_table "student_annotations", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_student_annotations_on_account_id"
+  end
+
+  create_table "student_entries", force: :cascade do |t|
+    t.integer "EntryNumber"
+    t.date "EntryDate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "task_statements", force: :cascade do |t|
     t.bigint "task_id", null: false
     t.bigint "statement_id", null: false
@@ -191,6 +205,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_02_193006) do
   add_foreign_key "help_examples", "accounts"
   add_foreign_key "solutions", "statements"
   add_foreign_key "statements", "users"
+  add_foreign_key "student_annotations", "accounts"
   add_foreign_key "task_statements", "statements"
   add_foreign_key "task_statements", "tasks"
   add_foreign_key "user_roles", "roles"

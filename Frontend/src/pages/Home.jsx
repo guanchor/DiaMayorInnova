bf7239e393ba1672142ece27396242d4e28fb312
AlbertoGG20 <albertoguerrerogutierrez@alumno.ieselrincon.es>
2024-12-15 15:20:10 +0000

    merge december - echedey, juan carlos and mayer branches

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { navContext } from '../context/nav-menu/navMenuContext';
 import Shortcuts from "../components/shortcuts/Shortcuts";

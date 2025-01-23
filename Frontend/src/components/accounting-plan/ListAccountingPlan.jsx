42f350d4fc78f23ffa4980047126fa391bd71f8d
jcbo2425 <jcbodaw2911@gmail.com>
2025-01-23 17:40:03 +0000

    Order table by Name column, and some css

--- Frontend/src/components/accounting-plan/ListAccountingPlan.jsx
+++ Frontend/src/components/accounting-plan/ListAccountingPlan.jsx
@@ -10,14 +10,13 @@ const AccountingPlansList = ({ newPGC }) => {
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [searchAccPlan, setSearchAccPlan] = useState("");
   const navigate = useNavigate();
+  const [sortOrder, setSortOrder] = useState("ascending") //Sort control state
 
 
 
   useEffect(() => {
     retrieveAccountingPlans();
-  }
-
-    , [newPGC]);
+  }, [newPGC]);
 
   const retrieveAccountingPlans = () => {
     AccountingPlanDataService.getAll()
@@ -29,7 +28,8 @@ const AccountingPlansList = ({ newPGC }) => {
       });
   };
 
-  const findByName = () => {
+  const findByName = (e) => {
+    e.preventDefault();
     if (searchAccPlan) {
       AccountingPlanDataService.findByName(searchAccPlan)
         .then(response => {
@@ -61,29 +61,50 @@ const AccountingPlansList = ({ newPGC }) => {
       });
   };
 
-
   const handleSearchChange = (e) => {
     setSearchAccPlan(e.target.value);
   };
+
+  //PGC sorted by Name column
+  const sortAccountinPlans = (order) => {
+    const sortedPGC = [...accountingPlans].sort((a, b) => {
+      if (order === "ascending") {
+        return a.name.localeCompare(b.name);
+      }
+      else {
+        return b.name.localeCompare(a.name);
+      }
+    });
+    setAccountingPlans(sortedPGC);
+  };
+
+  //Change order
+  const handleSortClick = () => {
+    const newOrder = sortOrder === "ascending" ? "descending" : "ascending";
+    setSortOrder(newOrder);
+    sortAccountinPlans(newOrder);
+  }
 
   return (
     <>
 
       <section className="accountingPlan__pgcList">
-        <h2 className="accountingPlan__header--h2">Todos los planes</h2>
+        <div className="accountingPlan__header">
+          <h2 className="accountingPlan__header--h2">Todos los planes</h2>
 
-        <div className="accountingPlan__input--filter">
-          <input
-            aria-label="Filtrar por nombre"
-            className="accountingPlan__input"
-            type="text"
-            value={searchAccPlan}
-            onChange={handleSearchChange}
-            onKeyDown={(e) => e.key === 'Enter' && findByName()} //Usability upgrade #1 -> Search pressing "Enter" key
-            placeholder="Filtrar por nombre"
-          />
-          <button className="btn accountingPlan__button" onClick={findByName}>Buscar</button>
+          <form className="search-bar search-bar--pgc" onSubmit={findByName}>
+            <input
+              aria-label="Filtrar por nombre"
+              className="search-bar_search" 
+              type="text"
+              value={searchAccPlan}
+              onChange={handleSearchChange}
+              placeholder="Filtrar por nombre"
+            />
+            <i className="fi fi-rr-search" onClick={findByName}></i>
+          </form>
         </div>
+        
 
         <div className="accountingPlan__table">
           {accountingPlans.length === 0 ? ( // Usability upgrade #3 -> Show message if there is no data
@@ -92,7 +113,10 @@ const AccountingPlansList = ({ newPGC }) => {
             <table className="accountingPlan_tbody">
               <thead>
                 <tr>
-                  <th>Nombre PGC</th>
+                  {/*Order table by Name column */}
+                  <th onClick={handleSortClick} style={{cursor: "pointer"}}>
+                    Nombre PGC {sortOrder === "ascending" ? <i className="fi fi-rr-angle-small-down"/> : <i className="fi fi-rr-angle-small-up"/>}
+                  </th>
                   <th>Acrónimo</th>
                   <th>Descripción</th>
                   <th>Acciones</th>

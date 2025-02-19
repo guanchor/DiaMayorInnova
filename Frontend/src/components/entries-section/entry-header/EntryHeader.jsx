09023926bf65cc9688661480581f3386ae920d76
AlbertoGG20 <albertoguerrerogutierrez@alumno.ieselrincon.es>
2025-02-19 19:06:18 +0000

    Css changes

--- Frontend/src/components/entries-section/entry-header/EntryHeader.jsx
+++ Frontend/src/components/entries-section/entry-header/EntryHeader.jsx
@@ -5,16 +5,12 @@ import "./EntryHeader.css"
 const EntryHeader = ({ addEntry, selectedStatement, examStarted }) => {
 
   const modalRef = useRef(null);
-  
+
   return (
     <div className='entry_header'>
       <h2>Asientos Contables</h2>
       <section className="entry_buttons">
-        <label className="entry_selector__label" htmlFor="entry_selector">Selector de asiento contable
-          <select className="entry_selector" name="entry_selector" id="entry_selector" >
-            <option value="">Seleccione un asiento</option>
-          </select>
-        </label>
+
         <button className='btn' disabled={!examStarted} onClick={() => {
           if (selectedStatement) {
             addEntry(selectedStatement.id);

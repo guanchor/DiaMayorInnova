5f683f6b569ca51d19f51c5b7d3ed81ca2ff146f
unknown <Echedey Henrquez>
2025-03-18 10:33:58 +0000

    add and delete console log

--- Frontend/src/components/class-group/ClassGroupList.jsx
+++ Frontend/src/components/class-group/ClassGroupList.jsx
@@ -32,6 +32,7 @@ const ClassGroupsList = ({ refreshTrigger, onEdit, onStudentCountChange, maxStud
   const fetchCurrentUser = async () => {
     try {
       const user = await userService.getCurrentUser();
+      console.log("PROBANDOOOO", user);
       setCurrentUser(user);
       if (user.role === "teacher") {
         const teacherGroups = await userService.getTeacherClassGroups(user.id);

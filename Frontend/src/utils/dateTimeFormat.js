cf78caca2ee10b0a439cdee668f809d6644c0d9a
Echedey Henríquez <echedeyhh1989@gmail.com>
2025-04-21 08:23:20 +0200

    Compensate for the time zone

--- Frontend/src/utils/dateTimeFormat.js
+++ Frontend/src/utils/dateTimeFormat.js
@@ -1,5 +1,8 @@
 const formatDateForInput = (date) => {
   const dateObj = new Date(date);
+  // Ajustamos la fecha para compensar el offset de la zona horaria
+  const offset = dateObj.getTimezoneOffset();
+  dateObj.setMinutes(dateObj.getMinutes() - offset);
   return dateObj.toISOString().slice(0, 16); // Devuelve el formato "yyyy-MM-ddThh:mm"
 };
 

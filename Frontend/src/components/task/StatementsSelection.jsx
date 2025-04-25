1697b7218b80efd547f0521a24474837416b0abb
AlbertoGG20 <albertoyt13@gmail.com>
2025-04-25 08:14:18 +0100

    WIP Class group and added searchBars

--- Frontend/src/components/task/StatementsSelection.jsx
+++ Frontend/src/components/task/StatementsSelection.jsx
@@ -145,7 +145,7 @@ const StatementsSelection = ({
   return (
     <section className="task-page__selection">
       <div className="task-page__selection--content">
-        {/* <div className="task-page__header-row">
+        <div className="task-page__header-row">
           <h2 className="task-page__header">Enunciados</h2>
         </div>
         <SearchBar
